e85396a0dd8a3b55ac057bb6c915e3db65e180e0
aaron montoya-moraga <montoyamoraga@gmail.com>
2022-02-09 16:39:10 -0300

    add comments

--- class-2022-guest-lecture-risd-intro-computation/risd_0_hello/risd_0_hello.ino
+++ class-2022-guest-lecture-risd-intro-computation/risd_0_hello/risd_0_hello.ino
@@ -1,9 +1,14 @@
+// setup() runs once, at the beginning
 void setup() {
+
+  // turn on serial port at baud rate of 9600
   Serial.begin(9600);
+
 }
 
+// loop() runs after setup(), until the end of times
 void loop() {
-  // put your main code here, to run repeatedly:
 
   Serial.println("hola!");
+
 }
